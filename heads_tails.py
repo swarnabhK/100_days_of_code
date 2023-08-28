86d7b33ff13a2eddc3ce424ea6fa7f8cb7c6990c
Swarnabh Kashyap <swarnabh202@gmail.com>
2023-08-27 23:21:13 -0400

    Added the file heads_tails.py

---
+++ heads_tails.py
@@ -0,0 +1,9 @@
+import random
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+#Write the rest of your code below this line 👇
+answer = random.randint(0,1)
+if(answer==0):
+    print("Tails")
+else:
+    print("Heads")
