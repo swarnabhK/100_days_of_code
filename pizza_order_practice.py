1bb62f0614228f50dfef0af7a7288939bc4401c0
Swarnabh Kashyap <swarnabh202@gmail.com>
2023-08-27 21:41:10 -0400

    Added the file pizza_order_practice.py

---
+++ pizza_order_practice.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+total = 0
+if(size=="S"):
+    total = 15
+    if(add_pepperoni=="Y"):
+        total+=2
+else:
+    if(size=="M"):
+        total = 20
+    else:
+        total = 25
+    if(add_pepperoni=="Y"):
+        total+=3
+if(extra_cheese=="Y"):
+    total+=1
+
+print(f"Your final bill is: ${total}.")
